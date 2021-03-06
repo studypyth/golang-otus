e29aeee616567e46abb178c7c53d35619a268241
Damir.Nugmanov <damir.nugmanov@tele2.kz>
2021-03-06 22:27:09 +0600

    hw05: Solved

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,15 +1,48 @@
-package hw05_parallel_execution //nolint:golint,stylecheck
+package hw05
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type Cnt struct {
+	mu  sync.Mutex
+	cnt int
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	// Place your code here
-	return nil
+func Run(tasks []Task, gortnNmb, errLimit int) error {
+	taskChan := make(chan Task, len(tasks))
+	guard := make(chan struct{}, gortnNmb)
+	errCnt := Cnt{}
+	wg := sync.WaitGroup{}
+	var res error
+	for _, task := range tasks { // Выдаем задания в канал тасков для воркеров и закрываем канал - заданий больше нет
+		if errCnt.cnt == errLimit {
+			res = ErrErrorsLimitExceeded
+			break
+		}
+		taskChan <- task
+		wg.Add(1)
+		guard <- struct{}{}
+		go func(task Task) {
+			worker(task, &wg, &errCnt)
+			<-guard
+		}(task)
+	}
+	wg.Wait()
+	return res
+}
+func worker(task Task, wg *sync.WaitGroup, errCnt *Cnt) {
+	defer wg.Done()
+	res := task()
+	if res != nil {
+		errCnt.mu.Lock()
+		errCnt.cnt++
+		errCnt.mu.Unlock()
+	}
 }
